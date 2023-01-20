80ca64a3481dc449888213c51b317def6c616e7d
Jonathan EUDE <jonathan.eude@gmail.com>
2023-01-20 15:52:59 +0100

    Mise à jour de la page 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,17 @@ function Home() {
   return (
     <div className="notfound-container">
       <div className="notfound-informations">
-        <span>404</span>
+        <img
+          src="/assets/images/Logo_Jonathan_EUDE_Consulting.png"
+          alt="Logo Jonathan EUDE Consulting"
+        />
+        <div>
+          <span>404</span>
+          <img
+            src="/assets/images/Jonathan_EUDE_Photo_Profil.png"
+            alt="Jonathan EUDE, Consultant / Chef de Projet / Développeur Freelance"
+          />
+        </div>
         <span>Oups! La page que vous demandez n'existe pas.</span>
       </div>
       <a href="/">Retourner sur la page d'accueil</a>
